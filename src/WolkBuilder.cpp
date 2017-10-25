a4302b0cd823e36d6d1d9eaf8d80fbb7b20e18c7
Sava Vranesevic <sava.vranesevic@wolkabout.com>
2017-10-25 14:36:50 +0200

    Persistence feature implemented
    Source reorganized from flat to tree structure
    
    MQTT messages are serialized from/to appropriate DTO objects instead of internal models
    
    Separation of responsibilities:
    ConnectivityService - Responsible for sending/receiving messages from WolkAbout IoT Platform
    PublishService - When data publish can be performed forwards readings to ConnectivityService
                     When data publish can not be performed, or fails, forwards readings to PersistService
    
    utilities/FileSystemUtils:
    Added file system utility functions
    
    utilities/StringUti;s:
    Added string utility functions
    
    README.md:
    Updated with new builder interface
    Updated with new persistence functionality

--- src/WolkBuilder.cpp
+++ src/WolkBuilder.cpp
@@ -17,10 +17,12 @@
 #include "WolkBuilder.h"
 #include "ActuationHandler.h"
 #include "ActuatorStatusProvider.h"
-#include "Device.h"
 #include "Wolk.h"
-#include "mqtt/MqttConnectivityService.h"
-#include "mqtt/PahoMqttClient.h"
+#include "model/Device.h"
+#include "service/connectivity/mqtt/MqttConnectivityService.h"
+#include "service/connectivity/mqtt/PahoMqttClient.h"
+#include "service/persist/PersistService.h"
+#include "service/persist/json/JsonPersistService.h"
 
 #include <functional>
 #include <string>
@@ -30,12 +32,6 @@ namespace wolkabout
 WolkBuilder& WolkBuilder::host(const std::string& host)
 {
     m_host = host;
-    return *this;
-}
-
-WolkBuilder& WolkBuilder::device(const Device& device)
-{
-    m_device = device;
     return *this;
 }
 
@@ -68,12 +64,21 @@ WolkBuilder& WolkBuilder::actuatorStatusProvider(std::weak_ptr<ActuatorStatusPro
     return *this;
 }
 
-std::unique_ptr<Wolk> WolkBuilder::build()
+WolkBuilder& WolkBuilder::withDataPersistence(std::shared_ptr<PersistService> persistService)
+{
+    m_persistService = persistService;
+    return *this;
+}
+
+std::unique_ptr<Wolk> WolkBuilder::build() const
 {
     auto mqttClient = std::make_shared<PahoMqttClient>();
     auto connectivityService = std::make_shared<MqttConnectivityService>(mqttClient, m_device, m_host);
 
-    auto wolk = std::unique_ptr<Wolk>(new Wolk(connectivityService, m_device));
+    auto publishService =
+      std::make_shared<PublishService>(connectivityService, m_persistService, std::chrono::milliseconds(50));
+
+    auto wolk = std::unique_ptr<Wolk>(new Wolk(publishService, m_device));
 
     wolk->m_actuationHandlerLambda = m_actuationHandlerLambda;
     wolk->m_actuationHandler = m_actuationHandler;
@@ -81,13 +86,18 @@ std::unique_ptr<Wolk> WolkBuilder::build()
     wolk->m_actuatorStatusProviderLambda = m_actuatorStatusProviderLambda;
     wolk->m_actuatorStatusProvider = m_actuatorStatusProvider;
 
+    connectivityService->setListener(
+      [&](const ActuatorCommand& actuatorCommand) -> void { wolk->handleActuatorCommand(actuatorCommand); });
+
     return wolk;
 }
 
-WolkBuilder::WolkBuilder() : m_host(WOLK_DEMO_HOST), m_device("", "", {}) {}
-
-WolkBuilder Wolk::newBuilder()
+wolkabout::WolkBuilder::operator std::unique_ptr<Wolk>() const
 {
-    return WolkBuilder();
+    return build();
+}
+
+WolkBuilder::WolkBuilder(Device device) : m_host(WOLK_DEMO_HOST), m_device(std::move(device)), m_persistService(nullptr)
+{
 }
 }
