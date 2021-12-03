1864eb7f0abf2c1d8f1c331525f1c66ab13cd539
nanavuletic <nenad.vuletic@wolkabout.com>
2021-12-03 15:24:31 +0100

    Started adding the debian package

---
+++ tools/make_deb.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Check what the current branch is
+if [ $# -eq 1 ]; then
+  branch=$1
+else
+  branch=$(git rev-parse --abbrev-ref HEAD)
+
+  if [ "$branch" == "" ]; then
+    echo "You must specify a branch as parameter to the script (if the script is not part of a repo)"
+    exit
+  fi
+fi
+
+# Get the git repository cloned
+mkdir -p tmp
+cd tmp || exit
+git clone --recurse-submodules https://github.com/Wolkabout/WolkConnect-Cpp.git
+
+# Enter the repo, checkout to branch, update submodules
+cd WolkConnect-Cpp || exit
+git checkout "$branch"
+git submodule update --recursive
+
+# Run the build
+debuild -us -uc -b -j"$(nproc)"
