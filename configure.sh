41ca31dbe46b78eaa8fea78cfbf77b8fb8e679cb
nanavuletic <nenad.vuletic@wolkabout.com>
2022-01-24 13:51:26 +0100

    Added the pre-commit check

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2021 WolkAbout Technology s.r.o.
+# Copyright 2022 Wolkabout Technology s.r.o.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +14,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Create the directory used
+mkdir -p out
+
+# Add the pre-commit hook to check code format with .clang-format
+cp tools/git/pre-commit .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
+
+# Enter the directory and start the CMake build
 pushd out || exit
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
-
+popd || exit
