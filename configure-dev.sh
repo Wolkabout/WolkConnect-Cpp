d0fa7d5261f4d272b2fd56ad2daecbe5f119668b
nanavuletic <ana.vuletic@wolkabout.com>
2022-05-24 09:57:01 +0200

    Added the `configure-dev` script for development configuration, and inlined all Copyright headers

--- configure-dev.sh
+++ configure-dev.sh
@@ -23,5 +23,5 @@ chmod +x .git/hooks/pre-commit
 
 # Enter the directory and start the CMake build
 pushd out || exit
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DBUILD_GTEST=OFF -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF ..
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DBUILD_GTEST=ON -DBUILD_TESTS=ON -DBUILD_EXAMPLES=ON ..
 popd || exit
